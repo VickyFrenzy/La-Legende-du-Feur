89147930613b89d40441a06d505d7f582b2a344f
Vicky Frenzy <vickyfrenzy@pm.me>
2025-10-28 19:19:15 +0100

    Add legendary coubeh

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,16 @@ client.once(Events.ClientReady, async (readyClient: Client) => {
 	console.log(`Ready! Logged in as ${readyClient.user!.tag}`);
 });
 
+const chooseReply = (): string => {
+	if (Math.random() < 0.001) {
+		return 'coubeh.';
+	}
+	return 'feur.';
+}
+
 client.on(Events.MessageCreate, async message => {
 	if (message.content.toLowerCase().includes('quoi')) {
-		await message.reply('feur.');
+		await message.reply(chooseReply());
 	}
 });
 
