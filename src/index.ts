6ea70913837ddf9a245834fdab8a83ae09f0724c
Vicky Frenzy <vickyfrenzy@pm.me>
2026-01-17 14:44:58 +0100

    Add graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,30 @@ client.on(Events.MessageCreate, async message => {
 });
 
 client.login(token);
+
+const gracefulShutdown = async (reason: string) => {
+	console.log(`Shutting down: ${reason}`);
+	try {
+		try {
+			client.destroy();
+		} catch (e) {
+			console.warn('Error destroying Discord client', e);
+		}
+		console.log('Shutdown complete');
+		process.exit(0);
+	} catch (err) {
+		console.error('Shutdown failed', err);
+		process.exit(1);
+	}
+};
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+process.on('unhandledRejection', (reason) => {
+	console.error('Unhandled Rejection:', reason);
+	void gracefulShutdown('unhandledRejection');
+});
+process.on('uncaughtException', (err) => {
+	console.error('Uncaught Exception:', err);
+	void gracefulShutdown('uncaughtException');
+});
